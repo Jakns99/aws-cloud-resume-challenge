7167f4f1753a9d9ae82bacfac9f122ba5c8c53f4
Jakns99 <Jakns@comcast.net>
2023-05-24 14:59:14 -0700

    Edited yaml code

--- infra/main.tf
+++ infra/main.tf
@@ -1,3 +1,85 @@
-resource "aws_lambda_function" "myfunc" {
-    filename       = data.archive_file
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source      = "${path.module}/lambda/"
+  output_path = "${path.module}/packedlambda.zip"
+}
+
+resource "aws_lambda_function_url" "url" {
+  function_name      = aws_lambda_function.myfunc.function_name
+  authorization_type = "NONE"
+
+  cors {
+    allow_credentials = true
+    allow_origins     = ["*"]
+    allow_methods     = ["*"]
+    allow_headers     = ["date", "keep-alive"]
+    expose_headers    = ["keep-alive", "date"]
+    max_age           = 86400
+  }
+}
+
+
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_policy" "cloud_resume_policy" {
+
+  name        = "aws_iam_policy_for_terraform_resume_project_policy"
+  path        = "/"
+  description = "AWS IAM Policy for managing the resume project role"
+    policy = jsonencode(
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Action" : [
+            "logs:CreateLogGroup",
+            "logs:CreateLogStream",
+            "logs:PutLogEvents"
+          ],
+          "Resource" : "arn:aws:logs:*:*:*",
+          "Effect" : "Allow"
+        },
+        {
+          "Effect" : "Allow",
+          "Action" : [
+            "dynamodb:UpdateItem",
+			"dynamodb:GetItem"
+          ],
+          "Resource" : "arn:aws:dynamodb:*:*:table/cloud-resume-challenge-counter"
+        },
+      ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach_iam_policy_to_iam_role" {
+  role = aws_iam_role.iam_for_lambda.name
+  policy_arn = aws_iam_policy.cloud_resume_policy.arn 
+}
+
+
+resource "aws_lambda_function" "lambda_function" {
+  filename         = data.archive_file.lambda_zip.output_path
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+  function_name    = "function"
+  role             = aws_iam_role.lambda_role.arn
+  handler          = "func.handler"
+  runtime          = "python3.10" 
 }
