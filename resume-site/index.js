474707141d8994be55e05b94ea1b3c504e23667b
Jakns99 <131918366+Jakns99@users.noreply.github.com>
2023-05-25 13:16:53 -0700

    Updated JS script

--- resume-site/index.js
+++ resume-site/index.js
@@ -1,6 +1,7 @@
 const counter = document.querySelector(".counter");
+
 async function updateCounter() {
-    let response = await fetch("https://ulc5lu5vlnej3hcqucqdqnkujy0mldzk.lambda-url.us-west-2.on.aws/");
+    let response = await fetch("https://r9se0ydrv1.execute-api.us-west-2.amazonaws.com/Prod");
     let data = await response.json();
     counter.innerHTML = ` Total Views: ${data}`;
 }
